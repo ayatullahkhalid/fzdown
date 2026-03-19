65c4464af120799b553fe7471b3513740ec9abaa
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-19 18:52:00 +0100

    edit

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Home = async () => {
   const scraper = new Scraper()
-  //await scraper.search("hello", "movies")
-  const seasonData = await scraper.getEpisodes(await scraper.getEpisodeList("files-One_Piece--2830.htm"), scraper.baseSeriesURL)
+  const seasonData = await scraper.search("hello", "movies")
+  
+  //await scraper.getEpisodes(await scraper.getEpisodeList("files-One_Piece--2830.htm"), scraper.baseSeriesURL)
 
   return (
     <div>
