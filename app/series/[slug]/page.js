38a36defaa183a6265c678797081c5e8c6601e52
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-19 16:08:45 +0100

    edit

--- app/series/[slug]/page.js
+++ app/series/[slug]/page.js
@@ -1,20 +1,30 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { usePathname } from "next/navigation"
 import SearchBar from "@/components/search";
 import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
 import { Spinner } from "@/components/ui/spinner"
-import { XCircleIcon } from '@phosphor-icons/react';
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { XCircleIcon, CopyIcon } from '@phosphor-icons/react';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import { Card, CardContent, CardHeader,  CardTitle } from "@/components/ui/card"
 
-export default function Show ({ params }) {
+export default function Show () {
   const pathname = usePathname()
   const slug = pathname.replace(/^\/series\//, "")
   const slugText = decodeURIComponent(slug).replace("subfolder-", "").replace(".htm", "")
   const [show, setShow] = useState({})
   const [loading, setLoading] = useState(false)
+  
+  const listRef = useRef(null)
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollLeft = 0
+    }
+  }, [])
   
   const fetchData = async () => {
     
@@ -34,7 +44,7 @@ export default function Show ({ params }) {
   useEffect(() => {
     if(!slug) return
     fetchData()
-  }, [])
+  }, [slug])
 
   const { title, desc, running, genres, seasons } = show
 
@@ -44,7 +54,7 @@ export default function Show ({ params }) {
       <div className="w-full py-2 px-2 br-2">
         <div className="results">
           {loading ? (
-            <Empty className="w-full bg-background">
+            <Empty theclassName="w-full bg-background">
               <EmptyHeader>
                 <EmptyMedia variant="icon" className="bg-transparent">
                   <Spinner className="bg-none"/>
@@ -76,13 +86,37 @@ export default function Show ({ params }) {
               <span>genres: {genres?.join(", ")}</span>
 
               <Tabs defaultValue={seasons?.[0]?.link || ""} className="w-full">
-                <TabsList className="w-full overflow-x-auto no-scrollbar" variant="line">
+                <TabsList ref={listRef} className="w-full overflow-x-auto no-scrollbar" variant="line">
                   {seasons?.map(({ title, link }) => (
                     <TabsTrigger key={link} value={link} className="whitespace-nowrap flex-shrink-0">
                       {title}
                     </TabsTrigger>
                   ))}
                 </TabsList>
+                {seasons?.map(({ title, link }) => (
+                  <TabsContent key={link} value={link}>
+                    <Card className="px-2">
+                      <CardHeader>
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            {Array.from({ length: 6 }).map((_, i) => (
+              <div className="flex flex-col gap-2">
+                <span className="font-bold text-lowercase">Title Here</span>
+                <span className="text-lowercase">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere sagittis risus a aliquet. Praesent nec semper tellus, sit amet mattis tellus. Donec ultrices, nisi vitae sollicitudin vulputate.</span>
+                <span className="flex">mp4: 
+                  <Button variant="outline" size="icon">
+                    <CopyIcon />
+                  </Button>
+                  <Button asChild variant="outline" size="xs"><Link href="/">Download</Link></Button>
+                </span>
+              </div>
+            )}
+          </CardContent>
+
+                    </Card>
+                  </TabsContent>
+                ))}
               </Tabs>
             </div>
           )}
