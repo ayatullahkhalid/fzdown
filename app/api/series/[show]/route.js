4ab92bff340b275704e7e3e18c0cd9f56fa514f5
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-19 08:58:00 +0100

    series?
    Tried series page for single shows lets see

---
+++ app/api/series/[show]/route.js
@@ -0,0 +1,18 @@
+import Scraper from "@/lib/scraper"
+
+export async function GET(req, {params}) {
+  const { show } = params
+  
+  if (!show) {
+    return Response.json({ show: [] })
+  }
+
+  try {
+    const scraper = new Scraper()
+    const result = await scraper.getShow(show)
+
+    return Response.json({ show : result })
+  } catch (err) {
+    return Response.json({ show: [] }, { status: 500 })
+  }
+}
---
+++ app/series/[slug]/page.js
@@ -0,0 +1,86 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { usePathname } from "next/navigation"
+import SearchBar from "@/components/search";
+import { Separator } from "@/components/ui/separator"
+import Link from "next/link"
+import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
+import { Spinner } from "@/components/ui/spinner"
+import { XCircleIcon } from '@phosphor-icons/react';
+
+export default function Show ({ params }) {
+  const { slug } = params
+  const [show, setShow] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = async () => {
+    try {
+      setLoading(true)
+      const res = await fetch(`/api/series/${slug}`)
+      const data = await res.json()
+      setShow(data.show || {})
+    } catch (err) {
+      console.error(err)
+      setShow({})
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const { title, desc, running, genres, seasons } = show
+
+  return(
+    <div className="cnt">
+      <SearchBar />
+      <div className="w-full py-2 px-2 br-2">
+        <div className="results">
+          {loading ? (
+            <Empty className="w-full">
+              <EmptyHeader>
+                <EmptyMedia variant="icon" className="bg-transparent">
+                  <Spinner className="bg-none"/>
+                </EmptyMedia>
+                <EmptyTitle>Loading Show</EmptyTitle>
+                <EmptyDescription>
+                  We'll get the data for you in a jiffy
+                </EmptyDescription>
+              </EmptyHeader>
+            </Empty>
+          ) : !show.title ? (
+            <Empty className="w-full absolute z-49 bg-background">
+              <EmptyHeader>
+                <EmptyMedia variant="icon" className="bg-transparent">
+                  <XCircleIcon />
+                </EmptyMedia>
+                <EmptyTitle>A problem occurred</EmptyTitle>
+                <EmptyDescription>
+                  We couldn't extract the show for you <br/>
+                  Check your internet connection or try again later
+                </EmptyDescription>
+              </EmptyHeader>
+            </Empty>
+          ) : (
+            <div className="flex w-full gap-2 px-2 flex-col">
+              <span className="text-sm font-bold">{title}</span>
+              <span>{desc}</span>
+              <span>aired: {running}, seasons: {seasons?.length}</span>
+              <span>genres: {genres?.join(", ")}</span>
+              <div className="flex flex-col gap-2 pl-2">
+                <span className="font-bold">Seasons</span>
+                {seasons?.map(({ title, link }) => (
+                  <Link key={link} href={`/series/${slug}/${link}`}>
+                    {title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
