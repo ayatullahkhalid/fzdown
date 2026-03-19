3b67c3d013e4d5ae567a210f76d53833f2c1104e
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-19 11:11:14 +0100

    hould work now

--- app/api/series/[show]/route.js
+++ app/api/series/[show]/route.js
@@ -3,7 +3,7 @@ import Scraper from "@/lib/scraper"
 export async function GET(req) {
   const url = new URL(req.url)
   const pathname = url.pathname
-  const show = pathname.replace(/^\/series\//, "")
+  const show = pathname.replace(/^\/api/series\//, "")
   
   if (!show) {
     return Response.json({ show: null })
