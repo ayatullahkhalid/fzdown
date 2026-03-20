626e10b8a9029a16bde043087038bd1a4648ae17
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-20 14:44:25 +0100

    edit, created api route for getting episodes data

---
+++ app/api/series/[show]/[season]/route.js
@@ -0,0 +1,26 @@
+import Scraper from "@/lib/scraper"
+
+export async function GET(req) {
+  const url = new URL(req.url)
+
+const pathname = url.pathname
+const parts = pathname.split("/").filter(Boolean)
+
+const season = parts[2]
+
+const start = url.searchParams.get("start")
+const end = url.searchParams.get("end")
+  
+  if (!season) {
+    return Response.json({ eps: null })
+  }
+
+  try {
+    const scraper = new Scraper()
+    const result = await scraper.getEpisodes(await scraper.getEpisodeList(season), scraper.baseSeriesURL, start, end)
+    
+    return Response.json({ eps : result })
+  } catch (err) {
+    return Response.json({ message: err.message, stack: err.stack, url: show}, { status: 500 })
+  }
+}
