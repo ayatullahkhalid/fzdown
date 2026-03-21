092984cbdbe1507e6a4a18c881a0d3644f49e86f
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-21 03:06:59 +0100

    edit

--- components/media-state.jsx
+++ components/media-state.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import EmptyState from "./empty-state"
 import { Spinner } from "@/components/ui/spinner"
 import { XCircleIcon } from "@phosphor-icons/react"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
 export default function MediaState({
   loading,
@@ -13,20 +14,35 @@ export default function MediaState({
   loadingDesc,
   errorTitle,
   errorDesc,
+  isSearch = false,
 }) {
   return loading ? (
     <EmptyState isError={false} title={loadingTitle} desc={loadingDesc} />
   ) : !results?.length ? (
     <EmptyState isError={true} title={errorTitle} desc={errorDesc} />
+  ) : isSearch ? (
+    <ScrollArea className="[&>div]:max-h-[50vh] pt-4 flex !absolute z-49 bg-muted">
+      <div className="w-full">
+        {results.map(({ title, desc, link }) => (
+          <React.Fragment key={link || i}>
+            {typeof children === "function"
+              ? children({ title, desc, link, type })
+              : children}
+          </React.Fragment>
+        ))}
+      </div>
+    </ScrollArea>
   ) : (
     <div className="flex">
       <div className="w-full">
         <div className="text-sm pl-2 pb-4">
           Latest {type === "series" ? "shows" : "movies"}
         </div>
-        {results.map(({title, desc, link}, i) => (
+        {results.map(({ title, desc, link }, i) => (
           <React.Fragment key={link || i}>
-            {typeof children === "function" ? children({title, desc, link, type}) : children}
+            {typeof children === "function"
+              ? children({ title, desc, link, type })
+              : children}
           </React.Fragment>
         ))}
       </div>
--- components/search.jsx
+++ components/search.jsx
@@ -3,12 +3,8 @@
 import { usePathname } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group"
-import { MagnifyingGlassIcon, XCircleIcon } from "@phosphor-icons/react"
+import { MagnifyingGlassIcon } from "@phosphor-icons/react"
 import { ScrollArea, ScrollBar } from "./ui/scroll-area"
-import { Separator } from "./ui/separator"
-import Link from "next/link"
-import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "./ui/empty"
-import { Spinner } from "./ui/spinner"
 
 const SearchBar = () => {
   const path = usePathname()
@@ -53,81 +49,64 @@ const SearchBar = () => {
   }, [])
 
   useEffect(() => {
-  if (!debQuery) return
+    if (!debQuery) return
 
-  const fetchData = async () => {
-    try {
-      setLoading(true)
+    const fetchData = async () => {
+      try {
+        setLoading(true)
 
-      if (isSeries || isMovie) {
-        const res = await fetch(`/api/search/${searchType}/${debQuery}`)
-        const data = await res.json()
-        setResults(data.results || [])
+        if (isSeries || isMovie) {
+          const res = await fetch(`/api/search/${searchType}/${debQuery}`)
+          const data = await res.json()
+          setResults(data.results || [])
+        }
+      } catch (err) {
+        console.error(err)
+        setResults([])
+      } finally {
+        setLoading(false)
       }
-    } catch (err) {
-      console.error(err)
-      setResults([])
-    } finally {
-      setLoading(false)
     }
-  }
 
-  fetchData()
-}, [debQuery, isSeries, isMovie])
-  
+    fetchData()
+  }, [debQuery, isSeries, isMovie])
+
   return (
     <div ref={containerRef} className="w-full py-4 px-2 br-2">
       <InputGroup>
-        <InputGroupInput id="search" placeholder={placeholder} value={query}
+        <InputGroupInput
+          id="search"
+          placeholder={placeholder}
+          value={query}
           onChange={(e) => setQuery(e.target.value)}
-          onFocus={() => setIsFocused(true)} />
+          onFocus={() => setIsFocused(true)}
+        />
         <InputGroupAddon align="inline-end">
           <MagnifyingGlassIcon />
         </InputGroupAddon>
       </InputGroup>
-      {showResults &&  (
+      {showResults && (
         <div className="results relative ">
-          {loading ? (
-            <Empty className="w-full !absolute z-49 bg-muted">
-              <EmptyHeader>
-                <EmptyMedia variant="icon" className="bg-transparent">
-                  <Spinner className="bg-none"/>
-                </EmptyMedia>
-                <EmptyTitle>Processing your request</EmptyTitle>
-                <EmptyDescription>
-                  Please wait while we process your request. Do not refresh the page.
-                </EmptyDescription>
-              </EmptyHeader>
-            </Empty>
-          ) : debQuery && results.length === 0 ? (
-              <Empty className="w-full !absolute z-49 bg-muted">
-                <EmptyHeader>
-                  <EmptyMedia variant="icon" className="bg-transparent">
-                    <XCircleIcon />
-                  </EmptyMedia>
-                  <EmptyTitle>No results found</EmptyTitle>
-                  <EmptyDescription>
-                    We couldnt find any results for {debQuery} <br/>
-                    Check your internet connection or try again later
-                  </EmptyDescription>
-                </EmptyHeader>
-              </Empty>
-          ) : (
-            <ScrollArea className="[&>div]:max-h-[50vh] pt-4 flex !absolute z-49 bg-muted">
-              <div className="w-full">
-                {results.map(({ title, desc, link }) => (
-                  <div key={link} className="w-full px-2">
-                    <Link className="flex flex-col gap-2" href={`/${searchType}/${link}` || "/"}
-                      onMouseDown={(e) => e.preventDefault()}>
-                      <span className="font-bold">{title}</span>
-                      <span>{desc}</span>
-                    </Link>
-                    <Separator className="my-4" />
-                  </div>
-                ))}
-              </div>
-            </ScrollArea>
-          )}
+          <MediaState
+            loading={loading}
+            results={results}
+            type={searchType}
+            isSearch={true}
+            loadingTitle="Processing your request"
+            loadingDesc="Please wait while we process your request. Do not refresh the page."
+            errorTitle="No results found"
+            errorDesc={
+              <>
+                We couldnt find any results for {debQuery} <br />
+                Check your internet connection or try again later
+              </>
+            }
+          >
+            {({ title, desc, link, type }) => (
+              <MediaList title={title} desc={desc} link={link} type={type} />
+            )}
+          </MediaState>
+          )
         </div>
       )}
     </div>
