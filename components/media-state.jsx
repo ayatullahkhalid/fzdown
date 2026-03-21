6c404cda9ceeb279ebbe341bb49d6bc263db773d
ayatullahkhalid <ayatullahkhalid1110@gmail.com>
2026-03-21 01:56:47 +0100

    edit

--- components/media-state.jsx
+++ components/media-state.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import { EmptyState } from "@/components/empty-state"
+import { EmptyState } from "./empty-state"
 import { Spinner } from "@/components/ui/spinner"
 import { XCircleIcon } from "@phosphor-icons/react"
 
@@ -15,16 +15,20 @@ export default function MediaState({
   errorDesc,
 }) {
   return loading ? (
-    <EmptyState isError="false" title={loadingTitle} desc={loadingDesc} />
+    <EmptyState isError={false} title={loadingTitle} desc={loadingDesc} />
   ) : !results?.length ? (
-    <EmptyState isError="true" title={errorTitle} desc={errorDesc} />
+    <EmptyState isError={true} title={errorTitle} desc={errorDesc} />
   ) : (
     <div className="flex">
       <div className="w-full">
         <div className="text-sm pl-2 pb-4">
           Latest {type === "series" ? "shows" : "movies"}
         </div>
-        {results.map((m) => children(m))}
+        {results.map((m, i) => (
+          <React.Fragment key={m.link || i}>
+            {typeof children === "function" ? children(m) : children}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   )
